3b6eb039418960a5209d604fbc21e72f93a9755b
wanghongcheng <shexing.w@gmail.com>
2015-05-02 18:35:29 +0800

    Add 3.58.c

---
+++ ch03/3.58.c
@@ -0,0 +1,38 @@
+/*************************************************************************
+	> File Name: 3.58.c
+	> Author: hcwang
+	> Mail: shexing.w@gmail.com 
+	> Created Time: Sat 02 May 2015 06:20:02 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+typedef enum {MODE_A, MODE_B, MODE_C, MODE_D, MODE_E} mode_t;
+
+int switch3(int *p1, int *p2, mode_t action){
+    int result = 0;
+    switch(action) {
+        case MODE_A:
+            result = *p1;
+            *p1 = *p2;
+            break;
+        case MODE_B:
+            result = *p1 + *p2;
+            *p2 = result;
+            break;
+        case MODE_C:
+            *p2 = 15;
+            result = *p1;
+            break;
+        case MODE_D:
+            *p2 = *p1;
+            break;
+        case MODE_E:
+            result = 17;
+            break;
+        default:
+            break;
+    }
+
+    return result;
+}
